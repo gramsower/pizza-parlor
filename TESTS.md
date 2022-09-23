61d08dfa53dd5968fc5853852c875d3124271288
gramsower <110431307+gramsower@users.noreply.github.com>
2022-09-22 23:07:44 -0700

    continue adding HTML, commit first iteration of orderPrice method, begin UI logic.

--- TESTS.md
+++ TESTS.md
@@ -3,6 +3,16 @@
 Describe: Pizza()
 
 Test: "It should return a Pizza object with four properties: customer name, size, toppings and price."
-Code: const myPizza = new Pizza("John", "medium", ["anchovies", "pineapple"], "$18.00");
-Expected Output: Pizza { customerName: "John", size: "medium", toppings: ["anchovies", "pineapple"], price: "18.00" }
+Code: const myPizza = new Pizza("John", "medium", ["ham", "pineapple"], "$20.00");
+Expected Output: Pizza { customerName: "John", size: "medium", toppings: ["ham", "pineapple"], price: "20.00" }
+
+Describe: orderPrice(size, topping)
+Test: "It should return the price of a pizza without any additional toppings."
+Code: const orderPrice = new orderPrice("medium")
+Expected output: 17
+
+Test: "It should return the price of a pizza with one additional topping."
+Code: const orderPrice = new orderPrice("medium", "2");
+Expected output: 20
+
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 // Business Logic for Pizza Order --------------
-function Pizza(customerName, pizzaSize, pizzaToppings, orderPrice) {
+function Pizza(customerName, pizzaSize, pizzaToppings) {
   this.customerName = customerName;
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
-  this.orderPrice = orderPrice;
+  this.price = this.orderPrice;
 }
 
 
+Pizza.prototype.orderPrice = function(pizzaSize, pizzaToppings) {
+  let price = 0;
+  if (pizzaSize === "small") {
+    price += 14;
+  } else if (pizzaSize === "medium") {
+    price += 17;
+  } else if (pizzaSize === "large") {
+    price +=20;
+  } else { 
+    price +=25;
+  } 
 
 
+  return price;
 
 
-
-
-
+};
 
 
 
 
 // UI Logic
+const pizza = new Pizza();
+
+function handleOrderSubmission(event) {
+  event.preventDefault();
+  const inputtedName = document.querySelector("input#order-name")
+window.addEventListener("load", function() {
